c3fc5202348ba55eeb8767b11942947ebc3ad9cd
adryelizondo <115176368+adryelizondo@users.noreply.github.com>
2023-02-25 08:52:55 -0600

    Se realizo el encapsulamiento de datos

--- Personaje.py
+++ Personaje.py
@@ -1,29 +1,41 @@
+from typing import Self
+
+
 class Personaje:
  
-      #definimos el constructor de personaje 
-	 def __init__(self, esp,nom,alt):
-		self.especie=esp
-		self.nombre=nom
-		self.altura=alt 
+#definimos el constructor de personaje 
+ def __init__(self,esp ,nom,alt):
+#encapsulamos nuestros atributos
+  self.__especie= esp
+  self.__nombre= nom
+  self.__altura= alt 
 
-
-	#atributos Personaje
-	especie="Humano"
-	nombre= "Master Chief"
-	altura= "2.70"
-
-	#Metodos Personaje
-
- 	def correr(self,status):
+#Metodos Personaje
+#Nombramos de nueva forma nuestros atributos 
+def correr(self,status):
 	if(status):
-	print("El personaje "+ self.nombre + "esta corriendo")
-	else: 
-	print("El personaje "+ self.nombre + "se detuvo")
+          print("El personaje "+ self.__nombre + "esta corriendo")
+        else: 
+         print("El personaje "+self.__nombre + "se detuvo")
 
- 	def lanzarGranadas(self):
-	print("El personaje "+ self.nombre + "Lanzo una granada")
+def lanzarGranadas(self):
+ print("El personaje "+ self.__nombre + "Lanzo una granada")
 
- 	def recargarArma(self, municiones):
+def recargarArma(self, municiones):
 	cargador=10
 	cargador= cargador + municiones
 	print("El arma tiene "+ str(cargador) + "balas") 
+def __pensar(self):
+    print("Estoy pensando :((( ")
+    
+#declarar getters and setters de atributos
+def getNombre(self):
+     return self.__nombre
+def setNombre(self, nom):
+     self.__nombre = nom
+def getEspecie (self,esp):
+     self.__especie = esp
+def getAltura(self):
+     return self.__altura
+def setAltura(self, alt):
+     self.__altura= alt 
--- objeto.py
+++ objeto.py
@@ -2,48 +2,44 @@
 from Personaje import *
 
 #1. Solicitar datos
-    print("")
-    print("####### Datos Heroe #")
-    especieH= input("Escribe la especie del Heroe: ")
-    nombreH= input("Escribe el nombre del Heroe: ")
-    alturaH= float("Escribe la altura del Heroe: ")
-    recargaH= int("ECuantas Balas recargas al Heroe: ")
+print("-----------Datos Heroe----------------")
+especieH= input("Escribe la especie del Heroe: ")
+nombreH= input("Escribe el nombre del Heroe: ")
+alturaH= float("Escribe la altura del Heroe: ")
+recargaH= int("Cuantas Balas recargas al Heroe: ")
 
-    print("")
-    print("####### Datos villano #")
-    especieV= input("Escribe la especie del Villano: ")
-    nombreV= input("Escribe el nombre del Villano: ")
-    alturaV= float("Escribe la altura del Villano: ")
-    recargaV= int("ECuantas Balas recargas al Villano: ")
+print("------------Datos del villano------------")
+especieV= input("Escribe la especie del Villano: ")
+nombreV= input("Escribe el nombre del Villano: ")
+alturaV= float("Escribe la altura del Villano: ")
+recargaV= int("Cuantas Balas recargas al Villano: ")
 
-    #2.Crear onjetos de clase personaje 
+#2.Crear onjetos de clase personaje 
 
-    heroe= Personaje(especieH,nombreH,alturaH)
-    villano= Personaje(especieV,nombreV,alturaV)
+heroe= Personaje(especieH,nombreH,alturaH)
+villano= Personaje(especieV,nombreV,alturaV)
 
+#3. Usar atributos y metodos
+#Ejemplo del set para ir al atributo 
 
-    #3. Usar atributos 
+heroe.setNombre(" Pepe Pecas ")
 
-    print("")
-    print("##### Objetos heroe ####")
-    print ("El personaje se llama: " + heroe.nombre)
-    print ("Pertenece a la especie: " + heroe.especie)
-    print ("y Tiene una altura de: " + str(heroe.altura))
-    heroe.correr(True)
-    heroe.lanzarGranadas()
-    heroe.recargarArma(recargaH)
-
-    print("")
-    print("##### Objetos Villano ####")
-     print ("El personaje se llama: " + villano.nombre)
-    print ("Pertenece a la especie: " + villano.especie)
-    print ("y Tiene una altura de: " + str(villano.altura))
-    villano.correr(False)
-    villano.lanzarGranadas()
-    villano.recargarArma(recargaV)
-
-#3. Usar metodos
-
+print("-----------Datos del heroe------------")
+print ("El personaje se llama: " + heroe.getNombre())
+print ("Pertenece a la especie: " + heroe.getEspecie())
+print ("y Tiene una altura de: " + str(heroe.GetAltura()))
 heroe.correr(True)
 heroe.lanzarGranadas()
-heroe.recargarArma(87)
+heroe.recargarArma(recargaH)
+
+#Ejemplo de un metodo privado
+#heroe.__pensar()
+
+print("-------------Datos Villano-------------")
+print ("El personaje se llama: " + villano.getNombre())
+print ("Pertenece a la especie: " + villano.getEspecie())
+print ("y Tiene una altura de: " + str(villano.getAltura()))
+villano.correr(False)
+villano.lanzarGranadas()
+villano.recargarArma(recargaV)
+
