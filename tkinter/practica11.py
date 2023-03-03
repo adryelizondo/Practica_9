b915f269ead952515efbd24881b4f45a764eaab7
adryelizondo <115176368+adryelizondo@users.noreply.github.com>
2023-03-03 11:50:12 -0600

    Funcionalidad de botones para agregar sus botones

--- tkinter/practica11.py
+++ tkinter/practica11.py
@@ -1,36 +1,43 @@
 from tkinter import Tk, Button, Frame, messagebox
 def mostrarMensajes():
     messagebox.showinfo("Aviso", "Presionaste el boton azul")
+    messagebox.showerror("Error", "Todo Fallo con exito")
+    print(messagebox.askesno("Pregunta", "¿El o ella jugo con tu corazon?"))
+    
+    #Agregar funcionalidad a un boton para agregar sus botones
+    def agregarBoton():
+        botonVerde.config(ext="+", bg="green", fg="white")
+        botonNuevo=Button(seccion3,text="Nuevo")
+        botonNuevo.pack()
 
+    #1. Instaciamos el objeto ventana
+    ventana= Tk()
+    ventana.title("Ejemplo de 3 frames ")
+    ventana.geometry("600x400")
 
-#1. Instaciamos el objeto ventana
-ventana= Tk()
-ventana.title("Ejemplo de 3 frames ")
-ventana.geometry("600x400")
+    #2. agregamos los frames
+    seccion1=Frame(ventana, bg="red") 
+    seccion1.pack(expand=True,fill="both")
 
-#2. agregamos los frames
-seccion1=Frame(ventana, bg="red") 
-seccion1.pack(expand=True,fill="both")
+    seccion2=Frame(ventana, bg="gray") 
+    seccion2.pack(expand=True,fill="both")
 
-seccion2=Frame(ventana, bg="gray") 
-seccion2.pack(expand=True,fill="both")
+    seccion3=Frame(ventana, bg="pink") 
+    seccion3.pack(expand=True,fill="both")
 
-seccion3=Frame(ventana, bg="pink") 
-seccion3.pack(expand=True,fill="both")
+    #3. Agregamos botones 
+    botonAzul= Button(seccion1,text="Boton Azul", fg="blue", command=mostrarMensajes)
+    botonAzul.place(x=60, y=60)
 
-#3. Agregamos botones 
-botonAzul= Button(seccion1,text="Boton Azul", fg="blue", command=mostrarMensajes)
-botonAzul.place(x=60, y=60)
+    botonNegro= Button (seccion2,text="Boton negro", fg="white", bg="black" )
+    botonNegro.grid(row=0, column=0)
 
-botonNegro= Button (seccion2,text="Boton negro", fg="white", bg="black" )
-botonNegro.grid(row=0, column=0)
+    botonAmarillo= Button (seccion2,text="Boton Amarillo",fg="white", bg="yellow")
+    botonAmarillo.grid(row=1, column=1)
 
-botonAmarillo= Button (seccion2,text="Boton Amarillo",fg="white", bg="yellow")
-botonAmarillo.grid(row=1, column=1)
+    botonVerde= Button (seccion3,text="Boton verde",fg="white", bg="green")
+    botonVerde.configure(height=2, width=10)
+    botonVerde.pack()
 
-botonVerde= Button (seccion3,text="Boton verde",fg="white", bg="green")
-botonVerde.configure(height=2, width=10)
-botonVerde.pack()
-
-#llamamos al main (el main loop se pone hasta el final del código)
-ventana.mainloop()
+    #llamamos al main (el main loop se pone hasta el final del código)
+    ventana.mainloop()
